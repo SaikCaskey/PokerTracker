e0b89afd492ce79578e00b27c0e968c8dbb9e398
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-09 01:53:20 +0200

    Update DriverFactory.kt

--- app/src/main/java/com/github/saikcaskey/pokertracker/domain/DriverFactory.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/domain/DriverFactory.kt
@@ -1,19 +1,7 @@
 package com.github.saikcaskey.pokertracker.domain
 
-import android.content.Context
-import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.android.AndroidSqliteDriver
-import com.github.saikcaskey.pokertracker.database.PokerTrackerDatabase
 
 interface DriverFactory {
     fun createDriver(): SqlDriver
-}
-
-
-
-fun createDatabase(driverFactory: DriverFactory): PokerTrackerDatabase {
-    val driver = driverFactory.createDriver()
-    return PokerTrackerDatabase(driver)
-//        .apply { SampleDataSeederImpl().seedSampleData(this) }
 }
