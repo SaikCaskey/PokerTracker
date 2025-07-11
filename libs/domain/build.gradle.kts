0fe4ae451d02a8c4a0a72d47c3fa8363db96349a
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-11 22:55:04 +0200

    Add decompose and sqlite dependncies for models

--- libs/domain/build.gradle.kts
+++ libs/domain/build.gradle.kts
@@ -29,7 +29,9 @@ kotlin {
 }
 
 dependencies {
+    implementation(libs.decompose.decompose)
     implementation(libs.kermit)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.datetime)
+    implementation(libs.sqldelight.android.driver)
 }
