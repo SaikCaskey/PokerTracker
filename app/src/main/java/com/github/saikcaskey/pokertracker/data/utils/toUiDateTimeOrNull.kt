7616e5c5cd24779365e9657b16de781e4b3fa1be
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-11 19:40:43 +0200

    Add/Update some utils

--- app/src/main/java/com/github/saikcaskey/pokertracker/data/utils/toUiDateTimeOrNull.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/data/utils/toUiDateTimeOrNull.kt
@@ -1,13 +1,11 @@
 package com.github.saikcaskey.pokertracker.data.utils
 
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.format.char
-import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.format.char
 
 fun Instant?.toUiDateTimeOrNull(): String? {
-    val localDateTime = this?.toLocalDateTime(TimeZone.currentSystemDefault()) ?: return null
+    val localDateTime = this?.asLocalDateTime() ?: return null
     val formatter = LocalDateTime.Format {
         year();char('-');monthNumber();char('-');dayOfMonth();char(' ')
         hour();char(':');minute()
