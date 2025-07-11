cfe8e4a688d47ecc8eb1402719988b616a10d571
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-11 23:19:26 +0200

    Update ExpenseMapper.kt

--- libs/data/src/main/java/com/github/saikcaskey/data/mappers/ExpenseMapper.kt
+++ libs/data/src/main/java/com/github/saikcaskey/data/mappers/ExpenseMapper.kt
@@ -1,5 +1,6 @@
 package com.github.saikcaskey.data.mappers
 
+import com.github.saikcaskey.pokertracker.domain.models.ExpenseType
 import com.github.saikcaskey.pokertracker.domain.util.asInstantOrNow
 import com.github.saikcaskey.pokertracker.domain.util.asInstantOrNull
 import com.github.saikcaskey.pokertracker.database.Expense
@@ -7,6 +8,7 @@ import com.github.saikcaskey.pokertracker.domain.models.Expense as DomainExpense
 
 fun Expense.toDomain(): DomainExpense {
     return DomainExpense(
+        id = id,
         eventId = event_id,
         venueId = venue_id,
         type = ExpenseType.valueOf(type),
