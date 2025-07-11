d8856127be54fc189c15ba5d373ad81aec2b10e0
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-11 22:56:16 +0200

    Create toExpenseColor.kt
    move these extensions to ui-compose

---
+++ libs/ui-compose/src/main/java/com/github/saikcaskey/pokertracker/ui_compose/extensions/toExpenseColor.kt
@@ -0,0 +1,25 @@
+package com.github.saikcaskey.pokertracker.ui_compose.extensions
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.github.saikcaskey.pokertracker.domain.models.Expense
+import com.github.saikcaskey.pokertracker.domain.models.ExpenseType
+
+@Composable
+fun Expense.toExpenseColor(): Color {
+    return when {
+        type == ExpenseType.CASH_OUT || type == ExpenseType.DEAL -> MaterialTheme.colorScheme.primary
+        amount == 0.0 -> MaterialTheme.colorScheme.secondary
+        else -> MaterialTheme.colorScheme.error
+    }
+}
+
+@Composable
+fun Double.toProfitColor(): Color {
+    return when {
+        this > 0.0 -> MaterialTheme.colorScheme.primary
+        this < 0.0 -> MaterialTheme.colorScheme.error
+        else -> MaterialTheme.colorScheme.secondary
+    }
+}
