8df456a94f575e3cdac34506a4ca4bc4a864196f
Alex Caskey <floreat.bangoria@gmail.com>
2025-07-11 23:23:05 +0200

    Update DatabaseModule.kt

--- app/src/main/java/com/github/saikcaskey/pokertracker/di/DatabaseModule.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/di/DatabaseModule.kt
@@ -1,11 +1,11 @@
 package com.github.saikcaskey.pokertracker.di
 
+import app.cash.sqldelight.db.SqlDriver
 import com.github.saikcaskey.data.database.DriverFactoryImpl
-import com.github.saikcaskey.pokertracker.domain.DriverFactory
 import com.github.saikcaskey.pokertracker.database.PokerTrackerDatabase
 import org.koin.dsl.module
 
 val databaseModule = module {
+    single<SqlDriver> { DriverFactoryImpl(get()).createDriver() }
     single<PokerTrackerDatabase> { PokerTrackerDatabase(get()) }
-    single<DriverFactory> { DriverFactoryImpl(get()) }
 }
