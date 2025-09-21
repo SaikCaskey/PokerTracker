7dac924fecbf5fbf111eb0580a15b65cb7f21422
Alex Caskey <floreat.bangoria@gmail.com>
2025-09-21 23:02:36 +0200

    Rename appModule -> appModules

--- app/src/main/java/com/github/saikcaskey/pokertracker/di/AppModule.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/di/AppModule.kt
@@ -2,8 +2,8 @@ package com.github.saikcaskey.pokertracker.di
 
 import org.koin.core.module.Module
 
-fun appModule(): List<Module> = listOf(
-    dispatchersProvidersModule,
+fun appModules(): List<Module> = listOf(
     databaseModule,
     repositoryModule,
+    dispatchersProvidersModule,
 )
