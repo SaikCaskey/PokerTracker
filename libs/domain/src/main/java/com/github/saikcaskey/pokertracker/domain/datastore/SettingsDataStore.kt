edf65c50ddd1ca3d15eb4c87266aaea5d7ecefcb
Alex Caskey <floreat.bangoria@gmail.com>
2025-09-21 22:30:12 +0200

    Create SettingsDataStore.kt

---
+++ libs/domain/src/main/java/com/github/saikcaskey/pokertracker/domain/datastore/SettingsDataStore.kt
@@ -0,0 +1,11 @@
+package com.github.saikcaskey.pokertracker.domain.datastore
+
+import com.github.saikcaskey.pokertracker.domain.models.SettingsData
+import kotlinx.coroutines.flow.Flow
+
+interface SettingsDataStore {
+
+    fun setUserId(userId: String? = null)
+
+    val data: Flow<SettingsData>
+}
