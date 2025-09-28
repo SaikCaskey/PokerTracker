21fae63e787bd3cd214ca360e272f8d66b5f11d6
Alex Caskey <floreat.bangoria@gmail.com>
2025-09-28 14:09:59 +0200

    Update StatsFeatureContent.kt

--- feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/StatsFeatureContent.kt
+++ feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/StatsFeatureContent.kt
@@ -1,5 +1,6 @@
 package com.github.saikcaskey.stats.presentation
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -17,5 +18,8 @@ fun StatsFeatureContent(
 fun StatsScreenContent(
     uiState: StatsFeatureComponent.UiState,
 ) {
-    Text("StatsScreenContent $uiState")
+    Column {
+        Text("StatsScreenContent $uiState")
+        
+    }
 }
