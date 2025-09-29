163449d1eeade90fd5ffa2e035464f825820c0b0
Alex Caskey <floreat.bangoria@gmail.com>
2025-09-29 13:15:09 +0200

    Update AccountFeatureComponentImpl.kt
    tidy up

--- feature/account/src/main/java/com/github/saikcaskey/account/presentation/AccountFeatureComponentImpl.kt
+++ feature/account/src/main/java/com/github/saikcaskey/account/presentation/AccountFeatureComponentImpl.kt
@@ -4,11 +4,13 @@ import com.arkivanov.decompose.ComponentContext
 import com.github.saikcaskey.database.utils.seedSampleData
 import com.github.saikcaskey.pokertracker.database.PokerTrackerDatabase
 import com.github.saikcaskey.pokertracker.domain.CoroutineDispatchers
-import com.github.saikcaskey.account.presentation.AccountFeatureComponent
 import com.github.saikcaskey.pokertracker.domain.models.UserPreference
 import com.github.saikcaskey.account.domain.repository.AccountSettingsRepository
+import com.github.saikcaskey.account.presentation.AccountFeatureComponent.UiState
+import com.github.saikcaskey.pokertracker.domain.models.UserPreference.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.SharingStarted.Companion
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -23,9 +25,9 @@ class AccountFeatureComponentImpl(
 
     private val coroutineScope = CoroutineScope(dispatchers.io)
 
-    override val uiState: StateFlow<AccountFeatureComponent.UiState> =
-        accountSettingsRepository.state.map(AccountFeatureComponent::UiState)
-            .stateIn(coroutineScope, SharingStarted.Companion.Eagerly, AccountFeatureComponent.UiState())
+    override val uiState: StateFlow<UiState> = accountSettingsRepository.state
+        .map(AccountFeatureComponent::UiState)
+        .stateIn(coroutineScope, Companion.Eagerly, UiState())
 
     override fun updatePreferenceValue(preference: UserPreference<*>, value: Any?) {
         @Suppress("UNCHECKED_CAST")
@@ -33,15 +35,15 @@ class AccountFeatureComponentImpl(
     }
 
     override fun setRandomUserId() {
-        accountSettingsRepository.setUserPreference(UserPreference.UserId, Uuid.Companion.random().toString())
+        accountSettingsRepository.setUserPreference(UserId, Uuid.Companion.random().toString())
     }
 
     override fun clearDefaultBuyIn() {
-        accountSettingsRepository.setUserPreference(UserPreference.DefaultBuyIn, null)
+        accountSettingsRepository.setUserPreference(DefaultBuyIn, null)
     }
 
     override fun clearUserId() {
-        accountSettingsRepository.setUserPreference(UserPreference.UserId, null)
+        accountSettingsRepository.setUserPreference(UserId, null)
     }
 
     override fun addDummyData() {
@@ -49,8 +51,11 @@ class AccountFeatureComponentImpl(
     }
 
     override fun clearAllData() {
-        database.eventQueries.deleteAll()
-        database.expenseQueries.deleteAll()
-        database.venueQueries.deleteAll()
+        database.transaction {
+            database.eventQueries.deleteAll()
+            database.userQueries.deleteAll()
+            database.venueQueries.deleteAll()
+            database.expenseQueries.deleteAll()
+        }
     }
 }
