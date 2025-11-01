7651f64dfb32196b3dc53de28f4bf55c99996e10
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-01 16:58:55 +0100

    Update VenueDetailContent.kt
    update venue detail to add some starting charts with the expense data

--- feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/VenueDetailContent.kt
+++ feature/stats/src/main/java/com/github/saikcaskey/stats/presentation/VenueDetailContent.kt
@@ -1,14 +1,32 @@
 package com.github.saikcaskey.stats.presentation
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.unit.*
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.github.saikcaskey.libs.ui_charts.domain.model.ChartDataItem
+import com.github.saikcaskey.libs.ui_charts.domain.model.ChartType
+import com.github.saikcaskey.libs.ui_charts.presentation.ChartyWidget
+import com.github.saikcaskey.pokertracker.domain.extensions.asLocalDateTime
 import com.github.saikcaskey.pokertracker.domain.extensions.toUiDateTimeOrNull
-import com.github.saikcaskey.pokertracker.domain.models.Event
+import com.github.saikcaskey.pokertracker.domain.models.EventSummary
 import com.github.saikcaskey.pokertracker.ui_compose.common.appbar.TopBarItemDetail
 import com.github.saikcaskey.pokertracker.ui_compose.common.profitsummary.AnimatedProfitText
 import com.github.saikcaskey.pokertracker.ui_compose.common.section.SectionContainer
@@ -17,10 +35,11 @@ import com.github.saikcaskey.pokertracker.ui_compose.extensions.toProfitColor
 import compose.icons.FontAwesomeIcons
 import compose.icons.fontawesomeicons.Solid
 import compose.icons.fontawesomeicons.solid.PlusCircle
+import kotlin.math.abs
 
 @Composable
 fun VenueDetailContent(modifier: Modifier = Modifier, component: VenueDetailComponent) {
-    val state by component.uiState.collectAsState()
+    val state by component.uiState.collectAsStateWithLifecycle()
 
     Scaffold(
         modifier = modifier,
@@ -52,11 +71,10 @@ fun VenueDetailContent(modifier: Modifier = Modifier, component: VenueDetailComp
         ) {
             item { VenueDetailSummary(state) }
             item { VenueProfitSummary(state) }
+            item { VenueExpenseSummary(state) }
             item {
                 VenueEventsSummary(
-                    upcomingEvents = state.upcomingEvents,
-                    todayEvents = state.todayEvents,
-                    pastEvents = state.pastEvents,
+                    eventSummary = state.eventSummary,
                     onEventClicked = component::onShowEventDetailClicked
                 )
             }
@@ -66,9 +84,7 @@ fun VenueDetailContent(modifier: Modifier = Modifier, component: VenueDetailComp
 
 @Composable
 fun VenueEventsSummary(
-    upcomingEvents: List<Event>,
-    pastEvents: List<Event>,
-    todayEvents: List<Event>,
+    eventSummary: EventSummary,
     onEventClicked: (Long) -> Unit,
 ) {
     SectionContainer(
@@ -76,21 +92,21 @@ fun VenueEventsSummary(
     ) {
         Text(text = "Upcoming", style = MaterialTheme.typography.labelLarge)
         EventsList(
-            items = upcomingEvents,
+            items = eventSummary.upcoming,
             emptyMessage = "",
             onEventClicked = onEventClicked,
         )
         Spacer(Modifier.height(8.dp))
         Text(text = "Today", style = MaterialTheme.typography.labelLarge)
         EventsList(
-            items = todayEvents,
+            items = eventSummary.today,
             emptyMessage = "",
             onEventClicked = onEventClicked,
         )
         Spacer(Modifier.height(8.dp))
-        Text(text = "Recent", style = MaterialTheme.typography.labelLarge)
+        Text(text = "All", style = MaterialTheme.typography.labelLarge)
         EventsList(
-            items = pastEvents,
+            items = eventSummary.all,
             emptyMessage = "",
             onEventClicked = onEventClicked,
         )
@@ -144,5 +160,51 @@ fun VenueProfitSummary(
         )
         Text("Total:", style = MaterialTheme.typography.titleMedium)
         AnimatedProfitText(state.profitSummary.balance)
+    }
+}
+
+@Composable
+fun VenueExpenseSummary(
+    state: VenueDetailComponent.UiState,
+    modifier: Modifier = Modifier,
+    onVenueClicked: ((Long) -> Unit)? = null,
+) {
+    val venue = state.venue
+    SectionContainer(
+        title = "Venue Expenses",
+        modifier = modifier
+            .fillMaxSize()
+            .clickable(venue?.id != null) {
+                venue?.id?.let { onVenueClicked?.invoke(it) }
+            }
+    ) {
+        val expenseCountData = state.expenseSummary.all.map {
+            ChartDataItem(
+                y = it.amount,
+                x = it.date?.asLocalDateTime()?.dayOfYear ?: 0
+            )
+        }
+        HorizontalDivider()
+        Text("Recent", style = MaterialTheme.typography.labelSmall)
+        HorizontalDivider()
+        ChartyWidget(dataPoints = expenseCountData, type = ChartType.Line)
+        HorizontalDivider()
+        ChartyWidget(dataPoints = expenseCountData, type = ChartType.Point)
+        HorizontalDivider()
+        val expenseTypeData = state.expenseSummary.all.associateBy { it.type }
+            .map {
+                ChartDataItem(
+                    label = it.key.name,
+                    y = it.value.amount,
+                )
+            }
+        Text("Breakdown", style = MaterialTheme.typography.labelSmall)
+        HorizontalDivider()
+        ChartyWidget(
+            dataPoints = expenseTypeData.map { it.copy(y = abs(it.y.toDouble())) },
+            type = ChartType.Pie
+        )
+        HorizontalDivider()
+        ChartyWidget(dataPoints = expenseTypeData, type = ChartType.Bar)
     }
 }
