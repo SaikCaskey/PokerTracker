e3856b334d562a7e599e35409780f31abb37b820
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-01 13:41:24 +0100

    Update build.gradle.kts
    opt in for expressive api

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ allprojects {
     tasks.withType<KotlinCompile> {
         compilerOptions {
             freeCompilerArgs.addAll(
+                "-opt-in=androidx.compose.material3.ExperimentalMaterial3ExpressiveApi",
                 "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
                 "-opt-in=com.arkivanov.decompose.ExperimentalDecomposeApi",
                 "-opt-in=com.arkivanov.decompose.DelicateDecomposeApi",
