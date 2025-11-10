291f582b1b1971bbc60195aa09bb909459ddc437
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-11 00:22:33 +0100

    Update ComponentFactoryModule.kt
    fix post merge imports

--- app/src/main/java/com/github/saikcaskey/pokertracker/di/ComponentFactoryModule.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/di/ComponentFactoryModule.kt
@@ -1,8 +1,8 @@
 package com.github.saikcaskey.pokertracker.di
 
 import androidx.annotation.Keep
-import com.github.saikcaskey.pokertracker.presentation.ComponentFactory
-import com.github.saikcaskey.pokertracker.presentation.ComponentFactoryImpl
+import com.github.saikcaskey.pokertracker.domain.factory.ComponentFactory
+import com.github.saikcaskey.pokertracker.presentation.components.factory.ComponentFactoryImpl
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import org.koin.dsl.module
