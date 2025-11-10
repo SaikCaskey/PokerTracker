9ae6095b2b66bf2d3092f2886ad720771b492b0a
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-11 00:08:42 +0100

    Remove flavors
    I forgot these aren't needed here

--- build-logic/convention/src/main/java/com/github/saikcaskey/pokertracker/build_logic/convention/plugins/ModuleComposeLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/github/saikcaskey/pokertracker/build_logic/convention/plugins/ModuleComposeLibraryConventionPlugin.kt
@@ -9,7 +9,6 @@ import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.co
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedBuildTypes
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedCompileOptions
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedDefaultConfig
-import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedFlavors
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedPackaging
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -30,7 +29,6 @@ class ModuleComposeLibraryConventionPlugin : Plugin<Project> {
                 configureSharedBuildTypes()
                 configureSharedBuildFeatures(enableCompose = true)
                 configureSharedPackaging()
-                configureSharedFlavors()
                 configureSharedCompileOptions()
                 configureSharedDefaultConfig(this)
             }
--- build-logic/convention/src/main/java/com/github/saikcaskey/pokertracker/build_logic/convention/plugins/ModuleLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/github/saikcaskey/pokertracker/build_logic/convention/plugins/ModuleLibraryConventionPlugin.kt
@@ -9,7 +9,6 @@ import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.co
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedBuildTypes
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedCompileOptions
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedDefaultConfig
-import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedFlavors
 import com.github.saikcaskey.pokertracker.build_logic.convention.sharedconfig.configureSharedPackaging
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -29,7 +28,6 @@ class ModuleLibraryConventionPlugin : Plugin<Project> {
                 configureSharedBuildTypes()
                 configureSharedBuildFeatures()
                 configureSharedPackaging()
-                configureSharedFlavors()
                 configureSharedCompileOptions()
                 configureSharedDefaultConfig(this)
             }
