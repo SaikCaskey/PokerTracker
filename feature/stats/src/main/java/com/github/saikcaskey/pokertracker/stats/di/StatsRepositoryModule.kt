b4e3480f3d3affab52bf3638cf3146f187941ae4
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-22 10:45:04 +0100

    Update StatsRepositoryModule.kt

--- feature/stats/src/main/java/com/github/saikcaskey/pokertracker/stats/di/StatsRepositoryModule.kt
+++ feature/stats/src/main/java/com/github/saikcaskey/pokertracker/stats/di/StatsRepositoryModule.kt
@@ -1,15 +1,13 @@
 package com.github.saikcaskey.pokertracker.stats.di
 
 import com.github.saikcaskey.pokertracker.domain.repository.EventRepository
-import com.github.saikcaskey.pokertracker.domain.repository.VenueRepository
 import com.github.saikcaskey.pokertracker.domain.repository.ExpenseRepository
 import com.github.saikcaskey.pokertracker.domain.repository.UserRepository
+import com.github.saikcaskey.pokertracker.domain.repository.VenueRepository
 import com.github.saikcaskey.pokertracker.stats.data.repository.EventRepositoryImpl
 import com.github.saikcaskey.pokertracker.stats.data.repository.ExpenseRepositoryImpl
 import com.github.saikcaskey.pokertracker.stats.data.repository.UserRepositoryImpl
 import com.github.saikcaskey.pokertracker.stats.data.repository.VenueRepositoryImpl
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import org.koin.dsl.module
 
 val statsRepositoryModule = module {
@@ -17,20 +15,4 @@ val statsRepositoryModule = module {
     single<ExpenseRepository> { ExpenseRepositoryImpl(get(), get()) }
     single<VenueRepository> { VenueRepositoryImpl(get(), get()) }
     single<UserRepository> { UserRepositoryImpl(get(), get()) }
-}
-
-object EventRepositoryProvider : KoinComponent {
-    fun provide(): EventRepository = get()
-}
-
-object ExpenseRepositoryProvider : KoinComponent {
-    fun provide(): ExpenseRepository = get()
-}
-
-object VenueRepositoryProvider : KoinComponent {
-    fun provide(): VenueRepository = get()
-}
-
-object UserRepositoryProvider : KoinComponent {
-    fun provide(): UserRepository = get()
 }
