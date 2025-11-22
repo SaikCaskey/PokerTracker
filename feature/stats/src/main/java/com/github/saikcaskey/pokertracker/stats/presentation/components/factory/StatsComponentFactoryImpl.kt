8aa0ec43a49a8e36c540d4624f6f9f818f6c97f7
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-22 11:26:44 +0100

    Update StatsComponentFactoryImpl.kt
    pass unused route for now anwya

--- feature/stats/src/main/java/com/github/saikcaskey/pokertracker/stats/presentation/components/factory/StatsComponentFactoryImpl.kt
+++ feature/stats/src/main/java/com/github/saikcaskey/pokertracker/stats/presentation/components/factory/StatsComponentFactoryImpl.kt
@@ -7,12 +7,12 @@ import com.github.saikcaskey.pokertracker.domain.presentation.navigation.RootNav
 import com.github.saikcaskey.pokertracker.domain.repository.EventRepository
 import com.github.saikcaskey.pokertracker.domain.repository.ExpenseRepository
 import com.github.saikcaskey.pokertracker.domain.repository.VenueRepository
-import com.github.saikcaskey.pokertracker.stats.domain.components.ViewEventsComponent
-import com.github.saikcaskey.pokertracker.stats.domain.components.ViewExpensesComponent
-import com.github.saikcaskey.pokertracker.stats.domain.components.ViewVenuesComponent
 import com.github.saikcaskey.pokertracker.stats.domain.components.factory.StatsComponentFactory
+import com.github.saikcaskey.pokertracker.stats.domain.components.ViewEventsComponent
 import com.github.saikcaskey.pokertracker.stats.presentation.components.ViewEventsComponentImpl
+import com.github.saikcaskey.pokertracker.stats.domain.components.ViewExpensesComponent
 import com.github.saikcaskey.pokertracker.stats.presentation.components.ViewExpensesComponentImpl
+import com.github.saikcaskey.pokertracker.stats.domain.components.ViewVenuesComponent
 import com.github.saikcaskey.pokertracker.stats.presentation.components.ViewVenuesComponentImpl
 import com.github.saikcaskey.pokertracker.stats.presentation.navigation.StatsPagerNavigationRoute
 
