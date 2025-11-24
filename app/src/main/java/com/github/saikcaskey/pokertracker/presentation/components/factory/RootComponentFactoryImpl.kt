7cfce191aa466f6654baa6cfd2304b5657d8f549
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-24 07:34:20 +0100

    Update RootComponentFactoryImpl.kt
    add callbacks for expenses/events for the containers

--- app/src/main/java/com/github/saikcaskey/pokertracker/presentation/components/factory/RootComponentFactoryImpl.kt
+++ app/src/main/java/com/github/saikcaskey/pokertracker/presentation/components/factory/RootComponentFactoryImpl.kt
@@ -204,8 +204,11 @@ class RootComponentFactoryImpl(
             venueId = venueId,
             dispatchers = dispatchers,
             onShowEventDetail = { navigator.push(RootNavigationRoute.EventDetailRoute(it)) },
+            onShowExpenseDetail = { navigator.push(RootNavigationRoute.ExpenseDetailRoute(expenseId = it)) },
             onShowInsertEvent = { navigator.push(RootNavigationRoute.InsertEventRoute(venueId = venueId)) },
             onShowEditVenue = { navigator.push(RootNavigationRoute.InsertVenueRoute(venueId = venueId)) },
+            // TODO show / open to events for a given venueId
+            onShowAllEvents = { navigator.push(RootNavigationRoute.ViewEventsRoute) },
             onFinished = navigator::pop,
             expenseRepository = expenseRepository,
             venueRepository = venueRepository,
