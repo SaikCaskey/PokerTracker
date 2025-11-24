b908d74d1fd932bdbad72f634e1752295aa3ac0c
Alex Caskey <floreat.bangoria@gmail.com>
2025-11-24 07:34:59 +0100

    Support donut widget
    add a donut type of chart (i.e., skinny pie)

--- libs/ui-charts/src/main/java/com/github/saikcaskey/libs/ui_charts/domain/model/ChartType.kt
+++ libs/ui-charts/src/main/java/com/github/saikcaskey/libs/ui_charts/domain/model/ChartType.kt
@@ -4,5 +4,6 @@ enum class ChartType {
     Point,
     Line,
     Pie,
+    Donut,
     Bar,
 }
--- libs/ui-charts/src/main/java/com/github/saikcaskey/libs/ui_charts/presentation/ChartyWidget.kt
+++ libs/ui-charts/src/main/java/com/github/saikcaskey/libs/ui_charts/presentation/ChartyWidget.kt
@@ -1,18 +1,15 @@
 package com.github.saikcaskey.libs.ui_charts.presentation
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import com.github.saikcaskey.libs.ui_charts.domain.model.ChartDataItem
+import com.github.saikcaskey.libs.ui_charts.domain.model.ChartType
 import com.github.saikcaskey.libs.ui_charts.presentation.charts.ChartyBarChart
 import com.github.saikcaskey.libs.ui_charts.presentation.charts.ChartyLineChart
 import com.github.saikcaskey.libs.ui_charts.presentation.charts.ChartyPieChart
 import com.github.saikcaskey.libs.ui_charts.presentation.charts.ChartyPointChart
-import com.github.saikcaskey.libs.ui_charts.domain.model.ChartDataItem
-import com.github.saikcaskey.libs.ui_charts.domain.model.ChartType
 
 @Composable
 fun ChartyWidget(
@@ -27,6 +24,7 @@ fun ChartyWidget(
         when (type) {
             ChartType.Point -> ChartyPointChart(dataPoints)
             ChartType.Pie -> ChartyPieChart(dataPoints)
+            ChartType.Donut -> ChartyPieChart(dataPoints, isDonut = true)
             ChartType.Line -> ChartyLineChart(dataPoints)
             ChartType.Bar -> ChartyBarChart(dataPoints)
         }
