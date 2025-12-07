5a99f6757d55a46eee44e637a447413eac74e341
Alex Caskey <floreat.bangoria@gmail.com>
2025-12-07 11:26:34 +0100

    Create AccountSettingsRepository.kt

---
+++ libs/domain/src/main/java/com/github/saikcaskey/pokertracker/libs/domain/repository/AccountSettingsRepository.kt
@@ -0,0 +1,12 @@
+package com.github.saikcaskey.pokertracker.libs.domain.repository
+
+import com.github.saikcaskey.pokertracker.libs.domain.models.AccountSettingsData
+import com.github.saikcaskey.pokertracker.libs.domain.models.UserPreference
+import kotlinx.coroutines.flow.StateFlow
+
+interface AccountSettingsRepository {
+
+    val state: StateFlow<AccountSettingsData>
+
+    fun setUserPreference(preference: UserPreference<*>, value: Any?)
+}
