590301aefcba6faa5284ae0002b37e4473ac5461
Alex Caskey <floreat.bangoria@gmail.com>
2025-12-07 08:52:35 +0100

    Update animated profit text to try some stuff
    harmonizing the colors and whatnot - I need to do a bit of a theme study I think

--- libs/ui-compose/src/main/java/com/github/saikcaskey/pokertracker/ui_compose/extensions/DoubleExt.kt
+++ libs/ui-compose/src/main/java/com/github/saikcaskey/pokertracker/ui_compose/extensions/DoubleExt.kt
@@ -3,12 +3,13 @@ package com.github.saikcaskey.pokertracker.ui_compose.extensions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.materialkolor.ktx.harmonize
 
 @Composable
 fun Double.toProfitColor(): Color {
     return when {
-        this > 0.0 -> MaterialTheme.colorScheme.primary
-        this < 0.0 -> MaterialTheme.colorScheme.error
-        else -> MaterialTheme.colorScheme.secondary
+        this > 0.0 -> Color.Green.harmonize(MaterialTheme.colorScheme.primary)
+        this < 0.0 -> Color.Red.harmonize(MaterialTheme.colorScheme.primary)
+        else -> Color.Blue.harmonize(MaterialTheme.colorScheme.primary)
     }
 }
